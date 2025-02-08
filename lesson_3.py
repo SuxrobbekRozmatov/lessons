e6874ea93ff64464866c2938bd3357ca649c05d0
SuxrobbekRozmatov <suhrrobbek06@gmail.com>
2025-02-08 15:37:46 +0500

    lesson_3

---
+++ lesson_3.py
@@ -0,0 +1,71 @@
+# pochtalar = ["user1@gmail.com", "user2yahoo.com", "user3@outlook.com"]
+# for email in pochtalar:
+#     if "@" in email:
+#
+#         print(f" To'g'ri email: {email}")
+# else:
+#     print(f"  Noto'g'ri email: {email} ")
+
+
+
+# parollar = ["password123", "Qwerty!", "admin", "StrongPass1!"]
+#
+# for parol in parollar:
+#     if len(parol) < 8:
+#         print(f"{parol}: Juda qisqa")
+#     elif not any(char.isdigit() for char in parol) or not any(char in "!@#$%^&*()-_=+" for char in parol):
+#         print(f"{parol}: Kuchsiz parol")
+#     else:
+#         print(f"{parol}: Kuchli parol")
+
+
+# o_havolar = [20, 22, 19, 24, 25, 23, 21]
+# for o_havo in o_havolar :
+#     if o_havo < 22 :
+#         print(f'{o_havo}C salqin kun')
+#     else:
+#         print(f'{o_havo}C iliq kun')
+#
+
+
+#
+# mavjud_taomlar = ["Osh", "Shashlik", "Manti", "Lag'mon"]
+# buyurtma = input("Buyurtmangizni kiriting: ").title()
+# if buyurtma in mavjud_taomlar:
+#     print("Buyurtmangiz qabul qilindi!")
+# else:
+#     print("Kechirasiz, bunday taom yo‘q.")
+
+
+# yoshlar = [16, 21, 17, 30, 25]
+# for yosh in yoshlar :
+#     if yosh < 18 :
+#         print(f'{yosh}  Yosh chegarasiga yetmagan')
+#     else:
+#         print(f'{yosh}  Hush kelibsiz')
+#
+#
+# xabarlar = ["Yangi xabar", "Batareya past", "Yangilanish mavjud"]
+# for xabar in xabarlar:
+#     if xabar == "Batareya past":
+#         print("Telefoningizni quvvatlang")
+#
+
+
+# fayllar = ["kitob.jpg", "ko_jiguli.mp3", "tabiat.jpg", "malohat.mp3", "iphone16.jpg"]
+# musiqalar = []
+# rasmlar = []
+#
+# for fayl in fayllar:
+#     if fayl.endswith(".jpg"):
+#         rasmlar.append(fayl)
+#     elif fayl.endswith(".mp3"):
+#         musiqalar.append(fayl)
+#
+#
+# print("Rasmlar:", rasmlar)
+# print("Musiqalar:", musiqalar)
+
+txt = input('isming nima')
+print(f'salom {txt}')
+
